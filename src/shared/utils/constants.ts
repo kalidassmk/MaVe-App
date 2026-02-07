d68542dcfed303a81243c132c798c18dd7da943a
Kalidass Mahalingam <kalidass.mks@gmail.com>
2026-02-07 19:11:57 +0530

    Create src/shared/utils/constants.ts file with application constants

---
+++ src/shared/utils/constants.ts
@@ -0,0 +1,9 @@
+// Application Constants
+
+export const APP_NAME = 'MaVe App';
+export const VERSION = '1.0.0';
+export const API_BASE_URL = 'https://api.maveapp.com';
+export const TIMEOUT = 5000; // Request timeout in milliseconds
+export const DEFAULT_LANGUAGE = 'en';
+
+// Other constants...
