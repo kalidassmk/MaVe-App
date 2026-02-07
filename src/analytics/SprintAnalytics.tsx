ace1f130bb615adc30c48eae7997d92cdba71d08
Kalidass Mahalingam <kalidass.mks@gmail.com>
2026-02-07 19:12:14 +0530

    Create SprintAnalytics screen component

---
+++ src/analytics/SprintAnalytics.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SprintAnalytics = () => {
+  return (
+    <div>
+      <h1>Sprint Analytics</h1>
+      {/* Add your analytics logic and display components here */}
+    </div>
+  );
+};
+
+export default SprintAnalytics;
