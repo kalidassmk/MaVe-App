189d39837beba57382d78447414e20f295146403
Kalidass Mahalingam <kalidass.mks@gmail.com>
2026-02-07 19:12:09 +0530

    Create JumpAnalytics component

---
+++ src/analytics/JumpAnalytics.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const JumpAnalytics = () => {
+    return (
+        <div>
+            <h1>Jump Analytics Screen</h1>
+            {/* Add your analytics components here */}
+        </div>
+    );
+};
+
+export default JumpAnalytics;
