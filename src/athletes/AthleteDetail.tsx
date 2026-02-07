97e355269a32decea6399254f2216ead146ce557
Kalidass Mahalingam <kalidass.mks@gmail.com>
2026-02-07 19:13:25 +0530

    Create AthleteDetail component

---
+++ src/athletes/AthleteDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AthleteDetail: React.FC = () => {
+    return (
+        <div>
+            <h1>Athlete Detail Screen</h1>
+            {/* Add additional details here */}
+        </div>
+    );
+};
+
+export default AthleteDetail;
