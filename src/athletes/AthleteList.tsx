11253bb792966dea366a1e942fc2a397f290c401
Kalidass Mahalingam <kalidass.mks@gmail.com>
2026-02-07 19:13:35 +0530

    Create AthleteList screen component

---
+++ src/athletes/AthleteList.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const AthleteList: React.FC = () => {
+    return (
+        <div>
+            <h1>Athlete List</h1>
+            {/* Add your athlete list display logic here */}
+        </div>
+    );
+};
+
+export default AthleteList;
